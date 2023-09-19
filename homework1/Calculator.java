597ce22ec01114c99c7163eb0c17195693e11997
innokenty <eazy-mf-e@mail.ru>
2023-09-19 17:10:52 +0300

    hw1

---
+++ homework1/Calculator.java
@@ -0,0 +1,47 @@
+package homework1;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    
+    public static double squareRootExtraction(double num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(num);
+    }
+
+    public static double calculateDiscount(double purchaseAmount, int discountAmount) {
+        if (purchaseAmount < 0 || discountAmount < 0) {
+            throw new ArithmeticException("Purchase amount and discount amount must be non-negative.");
+        }
+
+        double discount = (discountAmount * purchaseAmount) / 100.0;
+
+        return purchaseAmount - discount;
+    }
+}
